8c723e07a2b251a029059b80d149d4d39778295b
CesarClavijo01 <cesarclavijo778@gmail.com>
2024-06-23 20:17:15 -0500

    api fetches for rosters

---
+++ Frontend/src/api/rosters.js
@@ -0,0 +1,39 @@
+const baseURL = 'http://localhost:3000/api';
+
+//create new roster
+async function createNewRoster(token, wrestlerId, brandId){
+    try{
+        const response = await fetch(`${baseURL}/rosters/${wrestlerId}/${brandId}`,{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        })
+
+        const result = await response.json(); 
+
+        return result
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+
+async function getUserRosters(token){
+    try{
+        const response = await fetch(`${baseURL}/rosters`,{
+            headers:{
+                'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+            }
+        })
+
+        const result = await response.json()
+
+        return result
+    }
+    catch(err){
+        console.error(err)
+    }
+}
